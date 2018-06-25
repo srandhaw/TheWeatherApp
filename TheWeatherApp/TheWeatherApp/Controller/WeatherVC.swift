e07889cd3b55d2dae6a9431d46bee1a599e7755a
Sehajbir Randhawa <srandhaw@purdue.edu>
2018-06-25 10:20:22 -0400

    UI completed

---
+++ TheWeatherApp/TheWeatherApp/Controller/WeatherVC.swift
@@ -0,0 +1,30 @@
+//
+//  WeatherVC.swift
+//  TheWeatherApp
+//
+//  Created by Sehajbir Randhawa on 6/22/18.
+//  Copyright © 2018 Sehajbir. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+    
+    //Outlets
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var currentTempLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var currentWeatherImage: UIImageView!
+    @IBOutlet weak var currentWeatherType: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+    
+
+}
+
